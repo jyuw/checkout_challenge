ad808d9a9dd9bde1efa5bf0c9e3c8f0292f2924c
Jade <jywfang@gmail.com>
2018-01-25 11:18:07 +0100

    initialized Checkout and Promo_rules class

---
+++ lib/Checkout.rb
@@ -0,0 +1,31 @@
+class Checkout
+
+#As a market
+#In order to display our goods
+#We need to make a database
+
+#As a user
+#In order to be able to scan an item
+#We need to correspond the goods to prices
+
+#As a market
+#In order to promote sales
+#We need to be able to calculate sale prices
+
+#As a programmer
+#In order to give discounts
+#We need to calculate discounts
+
+#As a user
+#In order to check out
+#We need to display the correct final price with discount
+
+  def initialize(attrs = {})
+    @basket = []
+    @products = add_products()
+  end
+
+  def add_products
+
+  end
+end
---
+++ lib/promo_rules.rb
@@ -0,0 +1,5 @@
+class promo_rules
+  def rules(minimum, discount, product_code, price_drop)
+    
+  end
+end
